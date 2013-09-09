c4d809a57a821e22108b8653818654fa326f6a59
FootKjott <oscarmccully@gmail.com>
2013-09-09 19:12:16 -0400

    waitforpx functions

--- src/run.c
+++ src/run.c
@@ -1,18 +1,8 @@
-#include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <time.h>
 #include "futscript.h"
 
 int main()
 {
-	color c;
-	c.r = 0;
-	c.g = 0;
-	c.b = 0;
-	printf("start");
-	
-	printf("\ndone=%d", waitforpxnot(0, 0, c, 10, 10));
+	//Bot code here
 
 	return 0;
 }
